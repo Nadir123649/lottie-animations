7dcf2f920d6c711605037258fa85aaf48c206ccd
Nadir123649 <nadir.ali@novatoresols.com>
2024-10-10 21:51:05 +0500

    add css

--- src/components/Creative/index.jsx
+++ src/components/Creative/index.jsx
@@ -1,46 +1,11 @@
 import React from 'react';
-import Slider from 'react-slick';
-import MainImage from "../../images/Asset-7.png";
 
 function CreativeSection() {
-    const sliderSettings = {
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        speed: 2000,
-        dots: false,
-        arrows: false,
-        autoplaySpeed: 2000,
-        cssEase: "linear",
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1.5,
-                    slidesToScroll: 1,
-                }
-            }
-        ]
-    };
+
     return (
         <section className='bg-[#F1F1F1]'>
             <div className='container client-logo-conainer  mx-auto px-4'>
-                
+                Creaative slider
             </div>
         </section>
     );
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,12 +8,12 @@ function Footer() {
         <footer className="relative pt-5 md:pt-10">
             <img src={MainImageFooter} alt="" className="relative top-[1px]" />
             <div className="relative z-10 flex flex-col md:flex-row justify-between items-center px-5 pb-[20px] bg-black">
-                <p className="text-white text-sm mb-0 pt-2 pb-2 md:pb-0 md:pt-5">
+                <p className="text-white text-base mb-0 pt-2 pb-2 md:pb-0 md:mt-5">
                     © Constantinou. 2024. All rights reserved
                 </p>
                 <img src={umbrella} alt="umbrella" className="max-w-[80px] " />
-                <p className="text-white text-sm font-normal mb-0 pt-2 pb-2 md:pb-0 md:pt-5">
-                    <a href="mailto:info@constantinougroup.com" className="text-white text-sm font-normal no-underline" >
+                <p className="text-white text-base font-normal mb-0 pt-2 pb-2 md:pb-0 md:mt-5">
+                    <a href="mailto:info@constantinougroup.com" className="text-white text-base font-normal no-underline" >
                         info@constantinougroup.com
                     </a>
                 </p>
