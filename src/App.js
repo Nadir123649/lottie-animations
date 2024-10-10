24eebc7ea589b781aa32a9caf61d66f79d313400
Nadir123649 <nadir.ali@novatoresols.com>
2024-10-10 20:23:39 +0500

    add changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
     <div className="App">
       <div>
         <AnimatedHeader />
-        {/* <Hero /> */}
         <Innovative />
         <CreativeSection />
         <SliderSection />
--- src/components/Inovative/index.jsx
+++ src/components/Inovative/index.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 
+
 function Innovative() {
     return (
-        <div>
-            <h1>
-                We do everything imaginable in the world of creative
-                communications, from design and product creation
+        <section className="box fadeIn flex justify-center items-center min-h-[60vh] flex-col">
+            <h1 className="text-center text-4xl font-semibold leading-[50px]">
+                We do everything imaginable in the world of creative <br />
+                communications, from design and product creation <br />
                 to website and app development, digital marketing, and advertising.
             </h1>
-            <p>
+            <p className="text-center text-2xl font-normal leading-normal mt-[44px] max-w-[855px]">
                 Our core focus is bringing your brand to life in fresh, unorthodox, and often
                 provocative ways. With a team of some of the brightest, most passionate, and
                 tenacious creative problem solvers, along with our trusted partners, we collaborate
                 to express your brand’s values and personality through every channel that makes
                 sense—and even a few that don’t!
             </p>
-        </div>
+        </section>
     );
-
 }
 
-export default Innovative
+export default Innovative;
