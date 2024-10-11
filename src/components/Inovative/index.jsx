3a4b542666f51a1c6ddcebf408e14486c95a1209
Nadir123649 <nadir.ali@novatoresols.com>
2024-10-11 19:06:59 +0500

    add changes

--- src/components/Inovative/index.jsx
+++ src/components/Inovative/index.jsx
@@ -14,7 +14,7 @@ function Innovative() {
                     setHasAnimated(true);
                 }
             },
-            { threshold: 0.4 } 
+            { threshold: 0.4 }
         );
 
         if (sectionRef.current) {
@@ -26,11 +26,11 @@ function Innovative() {
                 observer.unobserve(sectionRef.current);
             }
         };
-    }, [hasAnimated]); 
+    }, [hasAnimated]);
 
     return (
-        <section ref={sectionRef} className="box fadeIn flex justify-center items-center min-h-[60vh] flex-col px-3 md:px-5 py-4">
-            <h1 className="text-center text-2xl md:text-4xl font-bold leading-normal md:leading-[50px]">
+        <section ref={sectionRef} className="box fadeIn flex justify-center items-center flex-col px-3 md:px-5 py-20">
+            <h1 className="text-center text-lg md:text-3xl font-bold leading-normal md:leading-[40px]">
                 We do everything imaginable in the world of creative <br className="hidden md:block" />
                 communications, from design and product creation <br className="hidden md:block" />
                 to website and app development, digital marketing, and advertising.
@@ -42,12 +42,12 @@ function Innovative() {
                     loop={false}
                     src={rightarrowAnimation}
                     // style={{ width: 200, height: 300 }}
-                    className='absolute left-[20px] bottom-[-80px] arrow-four'
-                    keepLastFrame={true} 
+                    className='absolute left-[20px] arrow-four w-full'
+                    keepLastFrame={true}
                 />
             )}
 
-            <p className="text-center text-xl md:text-2xl font-normal leading-normal mt-[30px] md:mt-[44px] max-w-full md:max-w-[855px] w-full">
+            <p className="text-center text-xl md:text-xl font-normal leading-normal mt-[30px] md:mt-[50px] max-w-full md:max-w-[855px] w-full">
                 Our core focus is bringing your brand to life in fresh, unorthodox, and often
                 provocative ways. With a team of some of the brightest, most passionate, and
                 tenacious creative problem solvers, along with our trusted partners, we collaborate
@@ -61,8 +61,8 @@ function Innovative() {
                     loop={false}
                     src={leftarrowAnimation}
                     // style={{ width: 300, height: 300 }}
-                    className='absolute right-[40px] bottom-[-90px] arrow-five' 
-                    keepLastFrame={true} 
+                    className='absolute right-[40px]  arrow-five'
+                    keepLastFrame={true}
                 />
             )}
         </section>
