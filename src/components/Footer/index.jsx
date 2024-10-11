99a4c2219068bc3873a4f8e389834222ce5ce0e1
Nadir123649 <nadir.ali@novatoresols.com>
2024-10-11 19:56:22 +0500

    add changes

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,14 +4,14 @@ import FooterBg from "../../images/Asset-12.png";
 
 function Footer() {
     return (
-        <footer className="relative ">
-            <img src={FooterBg} alt="" className="w-full relative top-[1px] h-[60px] " />
-            <div className="main-footer relative flex flex-col md:flex-row bg-black justify-between items-center px-5  pb-[20px]">
-                <p className="text-white text-base mb-0 pt-2 pb-2 md:pb-0 md:mt-5">
+        <footer className="relative">
+            <img src={FooterBg} alt="" className="w-full relative top-[1px] h-[90px] " />
+            <div className="main-footer relative flex flex-col md:flex-row bg-black justify-between items-center px-5 pb-[5px]">
+                <p className="text-white text-base mb-0  md:pb-0 ">
                     © Constantinou. 2024. All rights reserved
                 </p>
                 <img src={umbrella} alt="umbrella" className="max-w-[80px]" />
-                <p className="text-white text-base font-normal mb-0 pt-2 pb-2 md:pb-0 md:mt-5">
+                <p className="text-white text-base mb-0  md:pb-0 ">
                     <a
                         href="mailto:info@constantinougroup.com"
                         className="text-white text-base font-normal no-underline"
