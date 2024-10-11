bba4cbeffd2e8b42ead50774db225bba5324ac69
Nadir123649 <nadir.ali@novatoresols.com>
2024-10-11 19:10:13 +0500

    add changes

--- src/components/Inovative/index.jsx
+++ src/components/Inovative/index.jsx
@@ -29,7 +29,7 @@ function Innovative() {
     }, [hasAnimated]);
 
     return (
-        <section ref={sectionRef} className="box fadeIn flex justify-center items-center flex-col px-3 md:px-5 py-20">
+        <section ref={sectionRef} className="box fadeIn relative flex justify-center items-center flex-col px-3 md:px-5 py-20">
             <h1 className="text-center text-lg md:text-3xl font-bold leading-normal md:leading-[40px]">
                 We do everything imaginable in the world of creative <br className="hidden md:block" />
                 communications, from design and product creation <br className="hidden md:block" />
@@ -47,7 +47,7 @@ function Innovative() {
                 />
             )}
 
-            <p className="text-center text-xl md:text-xl font-normal leading-normal mt-[30px] md:mt-[50px] max-w-full md:max-w-[855px] w-full">
+            <p className="text-center text-sm md:text-xl font-normal leading-normal mt-[30px] md:mt-[50px] max-w-full md:max-w-[855px] w-full">
                 Our core focus is bringing your brand to life in fresh, unorthodox, and often
                 provocative ways. With a team of some of the brightest, most passionate, and
                 tenacious creative problem solvers, along with our trusted partners, we collaborate
@@ -60,8 +60,8 @@ function Innovative() {
                     autoplay
                     loop={false}
                     src={leftarrowAnimation}
-                    // style={{ width: 300, height: 300 }}
-                    className='absolute right-[40px]  arrow-five'
+
+                    className='absolute right-[40px] bottom-[-130px]  arrow-five w-full'
                     keepLastFrame={true}
                 />
             )}
