4a6cadc759a7adeb68a48f2f9c0f876c14673d02
Nadir123649 <nadir.ali@novatoresols.com>
2024-10-11 20:23:43 +0500

    add changes

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -12,14 +12,14 @@ function Footer() {
             </div>
             {/* <img src={FooterBg} alt="" className="w-full relative top-[1px] h-[90px] " /> */}
             <div className="main-footer relative flex flex-col md:flex-row bg-black justify-between items-center px-5 pb-[5px]">
-                <p className="text-white text-base mb-0  md:pb-0 ">
+                <p className="text-white text-sm mb-0  md:pb-0 ">
                     © Constantinou. 2024. All rights reserved
                 </p>
                 <img src={umbrella} alt="umbrella" className="max-w-[80px]" />
-                <p className="text-white text-base mb-0  md:pb-0 ">
+                <p className="text-white text-sm mb-0  md:pb-0 ">
                     <a
                         href="mailto:info@constantinougroup.com"
-                        className="text-white text-base font-normal no-underline"
+                        className="text-white text-sm font-normal no-underline"
                     >
                         info@constantinougroup.com
                     </a>
