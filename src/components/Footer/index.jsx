738b7beea100031f345bfd695eef7fe666bdb482
Nadir123649 <nadir.ali@novatoresols.com>
2024-10-12 14:52:09 +0500

    add css

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,7 +6,7 @@ function Footer() {
     return (
         <footer className="relative">
 
-            <div className="">
+            <div className="relative bottom-[-0.3px]">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 253.9">
                     <path d="M0,253.9C222.04,162.44,465.26,112,720.23,112s497.88,50.38,719.77,141.74V253.9H0Z" fill="#000000"></path>
                 </svg>
