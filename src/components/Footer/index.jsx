1d06f4bef01b4b6f27ad60a82feabe374dc6b207
Nadir123649 <nadir.ali@novatoresols.com>
2024-10-12 14:51:11 +0500

    add css

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,9 +6,9 @@ function Footer() {
     return (
         <footer className="relative">
 
-            <div className="relative">
+            <div className="">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 253.9">
-                    <path d="M0,253.9V0C222.12,91.46,465.34,141.9,720.31,141.9S1217.96,91.46,1440,0v253.9H0Z" fill="#000000"></path>
+                    <path d="M0,253.9C222.04,162.44,465.26,112,720.23,112s497.88,50.38,719.77,141.74V253.9H0Z" fill="#000000"></path>
                 </svg>
             </div>
             <div className="main-footer relative flex flex-col md:flex-row bg-black justify-between items-center px-5 pb-[5px]">
