bd44e9ae59ec676a2fb404caa92a72268a06b691
Nadir123649 <nadir.ali@novatoresols.com>
2024-10-12 15:03:00 +0500

    add css

--- src/components/Inovative/index.jsx
+++ src/components/Inovative/index.jsx
@@ -32,9 +32,9 @@ function Innovative() {
     return (
         <section
             ref={sectionRef}
-            className={`box fadeIn relative flex justify-center items-center flex-col px-3 md:px-5 py-20 ${hasAnimated ? 'fade-up' : ''}`}
+            className={`box fadeIn relative flex justify-center items-center flex-col px-3 md:px-5 pt-10 pb-20 ${hasAnimated ? 'fade-up' : ''}`}
         >
-            <h1 className="text-center text-lg md:text-2xl font-semibold leading-normal md:leading-[38px]">
+            <h1 className="text-center text-lg md:text-2xl font-normal leading-normal md:leading-[38px]">
                 We do everything imaginable in the world of creative <br className="hidden md:block" />
                 communications, from design and product creation <br className="hidden md:block" />
                 to website and app development, digital marketing, and advertising.
@@ -45,12 +45,12 @@ function Innovative() {
                     autoplay
                     loop={false}
                     src={rightarrowAnimation}
-                    className="absolute left-[20px] arrow-four w-full"
+                    className="absolute left-[25px] max-w-[640px] arrow-four w-full"
                     keepLastFrame={true}
                 />
             )}
 
-            <p className="text-center text-sm md:text-lg font-normal leading-normal mt-[30px] md:mt-[50px] max-w-full md:max-w-[855px] w-full">
+            <p className="text-center text-sm md:text-lg font-[400] leading-normal mt-[30px] md:mt-[50px] max-w-full md:max-w-[855px] w-full">
                 Our core focus is bringing your brand to life in fresh, unorthodox, and often
                 provocative ways. With a team of some of the brightest, most passionate, and
                 tenacious creative problem solvers, along with our trusted partners, we collaborate
@@ -63,7 +63,7 @@ function Innovative() {
                     autoplay
                     loop={false}
                     src={leftarrowAnimation}
-                    className="absolute right-[40px] bottom-[-130px] arrow-five w-full"
+                    className="absolute right-[40px] bottom-[-90px] max-w-[640px] arrow-five w-full"
                     keepLastFrame={true}
                 />
             )}
