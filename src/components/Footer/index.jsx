8522742b848b672798c52785ec494e149d951605
Nadir123649 <nadir.ali@novatoresols.com>
2024-10-12 14:54:22 +0500

    add css

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import umbrella from "../../images/umbrella.png";
-import FooterBg from "../../images/Asset-12.png";
 
 function Footer() {
     return (
         <footer className="relative">
-
             <div className="relative bottom-[-0.3px]">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 253.9">
                     <path d="M0,253.9C222.04,162.44,465.26,112,720.23,112s497.88,50.38,719.77,141.74V253.9H0Z" fill="#000000"></path>
                 </svg>
             </div>
             <div className="main-footer relative flex flex-col md:flex-row bg-black justify-between items-center px-5 pb-[5px]">
-                <p className="text-white text-sm mb-0  md:pb-0 ">
+                <p className="text-white text-sm mb-0  md:pb-0 pt-3">
                     © Constantinou. 2024. All rights reserved
                 </p>
                 <img src={umbrella} alt="umbrella" className="max-w-[80px]" />
-                <p className="text-white text-sm mb-0  md:pb-0 ">
+                <p className="text-white text-sm mb-0  md:pb-0 pt-3 ">
                     <a
                         href="mailto:info@constantinougroup.com"
                         className="text-white text-sm font-normal no-underline"
