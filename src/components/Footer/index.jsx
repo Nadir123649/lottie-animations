9c0caf999694b9539268c64127145696f717a785
Nadir123649 <nadir.ali@novatoresols.com>
2024-10-12 15:50:34 +0500

    add css

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,7 +4,7 @@ import umbrella from "../../images/umbrella.png";
 function Footer() {
     return (
         <footer className="relative">
-            <div className="relative bottom z-[-1]">
+            <div className="relative bottom-[-1px]">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 253.9">
                     <path d="M0,253.9C222.04,162.44,465.26,112,720.23,112s497.88,50.38,719.77,141.74V253.9H0Z" fill="#000000"></path>
                 </svg>
